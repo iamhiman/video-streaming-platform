18c44500fd3a2a0813044a17edcbf992688fdf7c
Himanshu Kashyap <himanshu27kashyap@gmail.com>
2024-03-10 10:37:11 +0530

    remove final set of broken urls from prod

--- src/constants.js
+++ src/constants.js
@@ -15,13 +15,7 @@ export const videoUrlsList = [
     url: "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.mp4/.m3u8",
   },
   { id: uuidv4(), url: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8" },
-  { id: uuidv4(), url: "https://moctobpltc-i.akamaihd.net/hls/live/571329/eight/playlist.m3u8" },
-  { id: uuidv4(), url: "https://new.ptvcrickethd.com/pak/media.stream/playlist.m3u8" },
   { id: uuidv4(), url: "https://assets.afcdn.com/video49/20210722/v_645516.m3u8" },
-  {
-    id: uuidv4(),
-    url: "https://res.cloudinary.com/dannykeane/video/upload/sp_full_hd/q_80:qmax_90,ac_none/v1/dk-memoji-dark.m3u8",
-  },
   {
     id: uuidv4(),
     url: "https://diceyk6a7voy4.cloudfront.net/e78752a1-2e83-43fa-85ae-3d508be29366/hls/fitfest-sample-1_Ott_Hls_Ts_Avc_Aac_16x9_1280x720p_30Hz_6.0Mbps_qvbr.m3u8",
